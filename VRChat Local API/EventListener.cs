b5d88ff618ee1275a3d665275218ecaa2cd46df3
zmke <zmke@xylo.dev>
2023-11-05 02:54:11 +0000

    Add project files.

---
+++ VRChat Local API/EventListener.cs
@@ -0,0 +1,28 @@
+﻿namespace VRChat_Local_API
+{
+    public class EventListener
+    {
+        private static string _logDirectory { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).Replace("Roaming", "LocalLow") + @"/VRChat/VRChat";
+        private static FileInfo? _logFile { get; set; } = null;
+        private static long _logFileCurrentLength { get; set; } = 0;
+
+        public void Initialize()
+        {
+            _logFile = GetLogFile();
+
+
+        }
+
+        public static FileInfo? GetLogFile()
+        {
+            foreach (var file in new DirectoryInfo(_logDirectory).GetFiles().OrderByDescending(x => x.LastWriteTime))
+            {
+                if (file.Name.EndsWith(".txt"))
+                {
+                    return file;
+                }
+            }
+            return null;
+        }
+    }
+}
