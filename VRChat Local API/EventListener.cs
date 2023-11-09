d1a6b25d622d5db43e2bae6ec73b52e0e53083fc
zmke <zmke@xylo.dev>
2023-11-09 03:01:07 +0000

    Basic EventListener Framework - The Events

--- VRChat Local API/EventListener.cs
+++ VRChat Local API/EventListener.cs
@@ -1,17 +1,71 @@
-﻿namespace VRChat_Local_API
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Text;
+using VRChat_Local_API.Objects;
+
+namespace VRChat_Local_API
 {
     public class EventListener
     {
         private static string _logDirectory { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).Replace("Roaming", "LocalLow") + @"/VRChat/VRChat";
-        private static FileInfo? _logFile { get; set; } = null;
-        private static long _logFileCurrentLength { get; set; } = 0;
+        private FileInfo? _logFile { get; set; } = null;
+        private long _logFileCurrentLength { get; set; } = 0;
+        private bool _disableThreads { get; set; } = false;
 
-        public void Initialize()
+        public string LogFileContent { get; set; } = string.Empty;
+        private string PastLogFileContent { get; set; } = "x";
+
+
+
+        public void Initialize(EventListenerConfig configuration)
         {
+            if (configuration.RequireGameRunning && !IsProcessRunning())
+                throw new Exception("VRChat isn't running check the active config");
+
             _logFile = GetLogFile();
 
+            if (_logFile != null)
+                using (FileStream fileStream = new FileStream(_logFile.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    _logFileCurrentLength = fileStream.Length;
+            else
+                throw new Exception("Failed to access the most recent log file");
 
+            StartEventThread();
         }
+
+        private void StartEventThread()
+        {
+            new Thread(() => 
+            {
+                while (!_disableThreads)
+                {
+                    using (FileStream fileStream = new FileStream(_logFile.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        fileStream.Seek(_logFileCurrentLength - 1, SeekOrigin.Begin);
+
+                        using (StreamReader streamReader = new StreamReader(fileStream, Encoding.Default))
+                        {
+                            LogFileContent = streamReader.ReadToEnd();
+                        }
+
+                        if (LogFileContent != PastLogFileContent)
+                        {
+                            foreach (var line in LogFileContent.Replace(PastLogFileContent, "").Split('\n'))
+                            {
+                           
+                            }
+
+                            PastLogFileContent = LogFileContent;
+                        }
+                    }
+                }
+            }).Start();
+        }
+
+        public void Shutdown() { _disableThreads = true; }
+
+        public bool IsProcessRunning() => Process.GetProcessesByName("VRChat").Length > 0;
 
         public static FileInfo? GetLogFile()
         {
---
+++ VRChat Local API/Objects/EventListenerConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRChat_Local_API.Objects
+{
+    public class EventListenerConfig
+    {
+        public bool DebugMode { get; set; } = true;
+        public bool RequireGameRunning { get; set; } = true;
+        public bool ListenToActiveEvent { get; set; } = true;
+    }
+}
